a83f6631c99f8c7be40844598b79bea3965b0da9
jzhang016 <JiaHua_Zhang@comcast.com>
2019-10-03 19:32:29 -0400

    enable to add new folder panel

--- src/Page/FolderPage.tsx
+++ src/Page/FolderPage.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Box } from 'grommet';
+import { Add } from 'grommet-icons';
 
 import { Files } from '../Component/Files/Files';
 import { ePub } from '../extension/ePub/ePub';
@@ -6,9 +8,16 @@ import { ePub } from '../extension/ePub/ePub';
 interface Props {}
 
 export const FolderPage: React.FC<Props> = () => {
+  const [state, setState] = useState([<Files openFileHandlers={[ePub]} />]);
+
+  const newFolderPanel = () => setState(prev => [...prev, <Files openFileHandlers={[ePub]} />]);
+
   return (
     <div>
-      <Files openFileHandlers={[ePub]} />
+      <Box align="start" pad="xsmall">
+        <Button icon={<Add />} label="New Folder Panel" onClick={newFolderPanel} primary />
+      </Box>
+      {state.map(s => s)}
     </div>
   );
 };
