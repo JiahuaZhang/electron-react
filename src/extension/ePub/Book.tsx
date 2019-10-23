68c863fdd9c14479847c0c4858265ec711cd2105
jzhang016 <JiaHua_Zhang@comcast.com>
2019-10-23 19:43:17 -0400

    update Book types

--- src/extension/ePub/Book.tsx
+++ src/extension/ePub/Book.tsx
@@ -1,3 +1,26 @@
+interface metadata {
+  ISBN: string;
+  UUID: string;
+  'calibre:timestamp': string;
+  'calibre:title_sort': string;
+  cover: string;
+  creator: string;
+  creatorFileAs: string;
+  date: string;
+  language: string;
+  publisher: string;
+  title: string;
+}
+
+interface manifest {
+  href: string;
+  id: string;
+  'media-type': string;
+  level?: number;
+  order?: number;
+  title?: number;
+}
+
 interface TocElement {
   level: number;
   order: number;
@@ -7,11 +30,21 @@ interface TocElement {
 }
 
 export interface EPub {
-  metadata: Object;
-  manifest: Object;
-  spine: Object;
-  flow: Object[];
+  metadata: metadata;
+  manifest: { [key: string]: manifest };
+  spine: {
+    contents: manifest[];
+    toc: { href: string; title: string; type: string };
+  };
+  flow: { [key: number]: manifest };
   toc: TocElement[];
+  containerFile: string;
+  filename: string;
+  guide: { href: string; title: string; type: string }[];
+  imageroot: string;
+  linkroot: string;
+  mimeFile: string;
+  rootFile: string;
 
   parse(): void;
 
