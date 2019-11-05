2f6680ca28830baa949c079c854034d0e1cf8e8f
jzhang016 <JiaHua_Zhang@comcast.com>
2019-11-04 19:25:53 -0500

    bug, fix import

--- src/extension/ePub/Screen.tsx
+++ src/extension/ePub/Screen.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Layout, Menu, Icon } from 'antd';
 
 import './Screen.sass';
