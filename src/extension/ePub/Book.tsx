9ac9657768351c2dcf8aa42fb72573f426617569
jzhang016 <JiaHua_Zhang@comcast.com>
2019-11-06 10:49:13 -0500

    roughly able to read the book for now

--- src/extension/ePub/Book.tsx
+++ src/extension/ePub/Book.tsx
@@ -2,33 +2,57 @@ import React, { useEffect, useState } from 'react';
 
 import { Section } from './Section';
 import { BookContext } from './BookContext';
+import { manifest } from './book.type';
 
-const img = (data: Buffer, mimeType: string, alt: string) => (
+export const img = (data: Buffer, mimeType: string, alt: string) => (
   <img alt={alt} src={`data:${mimeType};base64, ${data.toString('base64')}`} />
 );
 
 interface Props {}
 
 export const Book: React.FC<Props> = () => {
-  const [cover, setCover] = useState(<img alt="" />);
-
+  const [sections, setSections] = useState([{}] as {
+    flow: manifest;
+    section: null | JSX.Element;
+  }[]);
+  const [currentIndex, setCurrentIndex] = useState(0);
   const book = React.useContext(BookContext);
 
   useEffect(() => {
-    book.getImage(book.metadata.cover, (err, data, mimeType) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
+    const init_sections = book.flow.map(flow => ({
+      flow,
+      section: null as (null | JSX.Element)
+    }));
 
-      if (data) setCover(img(data, mimeType, 'cover'));
-    });
-  }, [book]);
+    init_sections[currentIndex].section = <Section section={init_sections[currentIndex].flow} />;
+    setSections(init_sections);
+  }, []);
+
+  useEffect(() => {
+    if (!sections[currentIndex].section) {
+      setSections(prev => {
+        const new_state = [...prev];
+        new_state[currentIndex].section = <Section section={new_state[currentIndex].flow} />;
+        return new_state;
+      });
+    }
+  }, [currentIndex]);
 
   return (
-    <div>
-      {cover}
-      <Section section={book.flow[0]} />
+    <div
+      tabIndex={0}
+      onKeyDown={event => {
+        if (event.key === 'ArrowRight') {
+          if (currentIndex + 1 < sections.length) {
+            setCurrentIndex(currentIndex + 1);
+          }
+        } else if (event.key === 'ArrowLeft') {
+          if (currentIndex - 1 >= 0) {
+            setCurrentIndex(currentIndex - 1);
+          }
+        }
+      }}>
+      {sections[currentIndex].section}
     </div>
   );
 };
