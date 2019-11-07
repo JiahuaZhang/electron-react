3bd199dbb428670b861dc9dcaaa5b6eb2e06070b
jzhang016 <JiaHua_Zhang@comcast.com>
2019-11-07 10:05:54 -0500

    now can load img as well

--- src/extension/ePub/Section.tsx
+++ src/extension/ePub/Section.tsx
@@ -7,18 +7,39 @@ interface Props {
   section: manifest;
 }
 
-const getImage = (book: EPub, fileName: string): Promise<string> => {
-  return new Promise((res, rej) => {
-    fileName = fileName.includes('.') ? fileName.split('.')[0] : fileName;
+const getImageString = (book: EPub, fileName: string): Promise<string> =>
+  new Promise((res, rej) => {
+    fileName = fileName.split('.')[0];
     book.getImage(fileName, (err, data, mimeType) => {
       if (err) {
-        console.error(`failed to getImage for ${fileName}`);
+        alert(`failed to getImage for ${fileName}`);
+        console.error(err);
         rej(err);
       }
+
       res(`data:${mimeType};base64, ${data.toString('base64')}`);
     });
   });
-};
+
+const getImgString = (book: EPub, fileName: string) =>
+  new Promise((res, rej) => {
+    const manifest = Object.values(book.manifest).find(value => value.href === fileName);
+    if (!manifest) {
+      alert(`fail to find img for ${fileName}`);
+      rej(`fail to find img for ${fileName}`);
+      return;
+    }
+
+    book.getImage(manifest.id, (err, data, mimeType) => {
+      if (err) {
+        alert(`failed to load image in book for ${fileName} with id: ${manifest.id}`);
+        console.error(err);
+        rej(err);
+      }
+
+      res(`data:${mimeType};base64, ${data.toString('base64')}`);
+    });
+  });
 
 export const Section: React.FC<Props> = ({ section }) => {
   const [html, setHtml] = useState(<></>);
@@ -41,8 +62,16 @@ export const Section: React.FC<Props> = ({ section }) => {
             for (const match of matches) {
               const attributes = match.match(/xlink:href="(.*)"/);
               if (!attributes) return;
-              const imageString = await getImage(book, attributes[1]);
+              const imageString = await getImageString(book, attributes[1]);
               text = text.replace(attributes[0], `xlink:href="${imageString}"`);
+            }
+          } else if (text.includes('<img')) {
+            const matches = text.match(/<img.*\/>/g) || [];
+            for (const match of matches) {
+              const attributes = match.match(/src=".*(images\/.*?)"/);
+              if (!attributes) return;
+              const imgString = await getImgString(book, attributes[1]);
+              text = text.replace(attributes[0], `src="${imgString}"`);
             }
           }
 
