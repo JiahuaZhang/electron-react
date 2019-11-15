c6f3f9b0e0fe29026d09a86efa131e18f57ec9e6
jzhang016 <JiaHua_Zhang@comcast.com>
2019-11-15 17:32:54 -0500

    update analyze

--- analyze.js
+++ analyze.js
@@ -1,16 +1,12 @@
-process.env.NODE_ENV = "production";
+process.env.NODE_ENV = 'production';
 
-const webpack = require("webpack");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
-  .BundleAnalyzerPlugin;
-const webpackConfigProd = require("react-scripts/config/webpack.config")(
-  "production"
-);
+const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const webpackConfigProd = require('react-scripts/config/webpack.config')('production');
 
 // this one is optional, just for better feedback on build
-const chalk = require("chalk");
-const ProgressBarPlugin = require("progress-bar-webpack-plugin");
-const green = text => chalk.green.bold(text);
+const chalk = require('chalk');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 
 // pushing BundleAnalyzerPlugin to plugins array
 webpackConfigProd.plugins.push(new BundleAnalyzerPlugin());
@@ -21,9 +17,9 @@ webpackConfigProd.plugins.push(new BundleAnalyzerPlugin());
 // and decide that compilation is kind of hang up on you; progress bar shows nice progression of webpack compilation
 webpackConfigProd.plugins.push(
   new ProgressBarPlugin({
-    format: `${green("analyzing...")} ${green("[:bar]")}${green(
-      "[:percent]"
-    )}${green("[:elapsed seconds]")} - :msg`
+    format: `${chalk.blue('analyzing...')} ${chalk.green(
+      '[:bar] [:percent] [:elapsed seconds]'
+    )} ${chalk.blue(':msg')}`
   })
 );
 
