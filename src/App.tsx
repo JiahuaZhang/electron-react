b9d7f473f70823af6909446d7d5a3b883bebf847
jzhang016 <JiaHua_Zhang@comcast.com>
2019-11-17 20:27:28 -0500

    init re-construct layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Tabs } from 'antd';
+import { Tabs, Layout, Icon, Menu } from 'antd';
 import 'antd/dist/antd.css';
 
 import './App.sass';
@@ -8,6 +8,7 @@ import { ePub } from './extension/ePub/ePub.handler';
 import { renderer } from './model/renderer';
 
 const { TabPane } = Tabs;
+const { Sider } = Layout;
 
 const App: React.FC = () => {
   const [tabs, setTabs] = useState<{ title: string; content: JSX.Element; key: number }[]>([]);
@@ -52,30 +53,37 @@ const App: React.FC = () => {
     }
   };
 
-  return tabs.length === 1 ? (
-    tabs[0].content
-  ) : (
-    <Tabs
-      className="main"
+  return (
+    <Layout
       style={{
-        height: '100vh',
         width: '100vw',
+        height: '100vh',
         display: 'grid',
-        gridTemplateRows: 'repeat(2, max-content)',
-        minHeight: 0,
-        minWidth: 0
-      }}
-      tabBarStyle={{ margin: 0 }}
-      type="editable-card"
-      onEdit={onEdit}
-      activeKey={activeKey}
-      onChange={activeKey => setActiveKey(activeKey)}>
-      {tabs.map(({ title, content, key }) => (
-        <TabPane tab={title} key={key.toString()}>
-          {content}
-        </TabPane>
-      ))}
-    </Tabs>
+        gridTemplateColumns: 'max-content 1fr'
+      }}>
+      <Layout
+        style={{
+          display: 'grid',
+          gridTemplateRows: 'max-content 1fr',
+          overflow: 'auto',
+          height: '100%'
+        }}>
+        <Icon
+          style={{ fontSize: '1.5rem', background: 'white', padding: '.5rem', textAlign: 'left' }}
+          type="fullscreen"
+        />
+        <Sider theme="light" collapsible>
+          <Menu defaultSelectedKeys={['home']}>
+            <Menu.Item key="home">
+              <Icon type="home" />
+            </Menu.Item>
+          </Menu>
+        </Sider>
+      </Layout>
+      <Layout style={{ overflow: 'auto', background: 'white' }}>
+        <FolderPage />
+      </Layout>
+    </Layout>
   );
 };
 
