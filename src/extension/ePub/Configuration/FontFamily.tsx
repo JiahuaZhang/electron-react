a8f9dac668cebf5dcdf7e6090d9887c5e6e51950
jzhang016 <JiaHua_Zhang@comcast.com>
2019-12-03 12:00:31 -0500

    update font style for English Font Section

--- src/extension/ePub/Configuration/FontFamily.tsx
+++ src/extension/ePub/Configuration/FontFamily.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { Tabs } from 'antd';
+import { Tabs, Radio } from 'antd';
+
+import './FontFamily.sass';
 
 const { TabPane } = Tabs;
 
@@ -12,32 +14,32 @@ export const FontFamily: React.FC<Props> = () => {
 
   return (
     <Tabs>
-      <TabPane tab="English" key="english">
-        {default_english_fonts.map(font => (
-          <div key={`english-${font}`}>
-            <label>
-              <input
-                type="radio"
-                name="english"
-                value={font}
-                onClick={event => {
-                  const { value } = event.target as HTMLInputElement;
-                  if (value === english_font) {
-                    (event.target as HTMLInputElement).checked = false;
-                    setEnglish_font('');
-                  } else {
-                    setEnglish_font(value);
-                  }
-                }}
-              />
+      <TabPane tab="Font" key="english">
+        <Radio.Group buttonStyle="solid" value={english_font}>
+          {default_english_fonts.map(font => (
+            <Radio.Button
+              key={`english-${font}`}
+              value={font}
+              onClick={event => {
+                event.persist();
+                const { value } = event.target as HTMLInputElement;
+                if (value === english_font) {
+                  setEnglish_font('');
+                } else {
+                  setEnglish_font(value);
+                }
+              }}
+              style={{
+                display: 'block',
+                margin: '5px'
+              }}>
               {font}
-            </label>
-            <br />
-          </div>
-        ))}
+            </Radio.Button>
+          ))}
+        </Radio.Group>
         <p style={{ fontFamily: english_font }}>The quick brown fox jumped over the lazy dog</p>
       </TabPane>
-      <TabPane tab="中文" key="chinese">
+      <TabPane tab="字体" key="chinese">
         <ul>
           <li>font1</li>
           <li>font2</li>
