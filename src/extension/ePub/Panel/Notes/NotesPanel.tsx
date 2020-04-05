2e6265ee1532f1bbfd63a73b3c2f55c194321ad6
jzhang016 <JiaHua_Zhang@comcast.com>
2020-04-04 22:09:50 -0400

    add notes panel, and it's now copyable

--- src/extension/ePub/Panel/Notes/NotesPanel.tsx
+++ src/extension/ePub/Panel/Notes/NotesPanel.tsx
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import { Button } from 'antd';
+import React, { useContext, useRef } from 'react';
+import { Button, notification } from 'antd';
 import { CopyOutlined } from '@ant-design/icons';
 
 import { NotesContext } from './NotesHook';
@@ -11,28 +11,41 @@ interface Props {}
 export const NotesPanel = (props: Props) => {
   const { fontSize, chinese_font, english_font } = React.useContext(ConfigContext);
   const { state } = useContext(NotesContext);
+  const ref = useRef<HTMLDivElement>(null);
 
-  const fontFamily = english_font
-    ? `${english_font}, ${chinese_font}`
-    : default_english_fonts.concat(chinese_font || '').join(',');
+  const fontFamily = [english_font, chinese_font]
+    .filter(Boolean)
+    .concat(default_english_fonts)
+    .join(',');
 
   return (
     <div style={{ display: 'grid' }}>
-      <Button style={{ justifySelf: 'center', marginTop: '.5rem' }}>
+      <Button
+        style={{ justifySelf: 'center', marginTop: '.5rem' }}
+        onClick={() => {
+          const range = document.createRange();
+          range.selectNode(ref.current as Node);
+          const selection = document.getSelection();
+          selection?.addRange(range);
+          const status = document.execCommand('copy');
+          if (status) {
+            notification.success({ message: 'copied!', duration: 1.5 });
+          } else {
+            notification.error({ message: 'failed to copy', duration: 1.5 });
+          }
+          navigator.clipboard.readText().then(console.log);
+          selection?.removeAllRanges();
+        }}>
         <CopyOutlined />
         copy all
       </Button>
-      <ul
-        style={{
-          listStyle: 'none',
-          padding: '.4rem',
-          fontFamily,
-          fontSize: fontSize && fontSize * 0.8
-        }}>
-        {state.map(({ text, backgroundColor }) => (
-          <li style={{ backgroundColor, margin: '.4rem auto' }}>{text}</li>
+      <div ref={ref} style={{ fontFamily, fontSize: fontSize && fontSize * 0.8 }}>
+        {state.map(({ text, backgroundColor }, index) => (
+          <p key={`${index}-${text}`} style={{ backgroundColor, margin: '.4rem auto' }}>
+            {text}
+          </p>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
